c3a382dda0dfe1582b9e8646bd30a2ae4ac04b7a
Mimoriam <mimoriam420@gmail.com>
2022-04-20 21:01:01 +0500

    Commit #4 - Create superuser and some bugfixes

--- tasks/models.py
+++ tasks/models.py
@@ -6,3 +6,6 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
